e5bff8e79a03cb12bac98ce686e6ab6fe2c02e21
Vijaykannnan <vijaykannan805@gmail.com>
2022-12-30 22:48:34 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/landing-page-via-react/precache-manifest.a5245311db5e13b1f216b0e8bee0abff.js"
+  "/React-Ui/precache-manifest.5eeed003a5277280c737675892a7b192.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/landing-page-via-react/index.html", {
+workbox.routing.registerNavigationRoute("/React-Ui/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
