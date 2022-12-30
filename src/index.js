c8dc31dcc9180e7363dcda35eaf2a69085e1fdf6
Vijaykannnan <vijaykannan805@gmail.com>
2022-12-30 22:27:15 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDom from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-ReactDom.render(<BrowserRouter><App /></BrowserRouter>, document.querySelector("#root"))
+ReactDOM.render((
+    <BrowserRouter basename={ process.env.PUBLIC_URL }>
+        <App />
+    </BrowserRouter>
+), document.querySelector("#root"))
